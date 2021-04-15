14d6b2b9efbd8b8da0ddc95b660b451f77062c8b
AKHILESHPATIDAR01 <akhilesh.forsignin@gmail.com>
2021-04-15 12:20:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ListItem from "./Components/ListItem";
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const newTask = (evt) => {
+    setNewItem(evt.target.value);
+  };
+  const saveItem = () => {
+    if (newItem.length > 0) {
+      items.push(newItem);
+      setItems([...items]);
+      setNewItem("");
+    }
+  };
+  const removeItem = (idx) => {
+    items.splice(idx, 1);
+    setItems([...items]);
+  };
+  const updateItem = (idx, value) => {
+    items[idx] = value;
+    setItems([...items]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      
+      <div className='textarea'>
+            <h1>To-do List</h1>
+            <textarea
+              id="task"
+              type="text"
+              placeholder="New Item"
+              value={newItem}
+              onChange={newTask}
+            ></textarea>
+            <button id="btn" onClick={saveItem}>
+              Add Task
+            </button>
+      </div>
+    
+
+      <div className='container-list'>
+          <div className='todo-items'>
+              <div className='title'>todo</div>
+              {items.map((item, idx) => (
+                <ListItem
+                  item={item}
+                  key={idx}
+                  index={idx}
+                  removeItem={removeItem}
+                  updateItem={updateItem}
+                />
+              ))}
+          </div>
+
+          <div className='inprogress-items'>
+            <div className='title'>inprogress</div>
+                  
+          </div>
+
+          <div className='done-items'>
+            <div className='title'>done</div>
+          
+          </div>
+      </div>
     </div>
   );
 }
